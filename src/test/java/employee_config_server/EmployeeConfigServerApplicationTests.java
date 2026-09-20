e06a621ef2d1fd34e16fddc03d03d32b926be2b0
Anil <anilrevoor93@gmail.com>
2026-09-21 00:02:55 +0530

    Config Server

---
+++ src/test/java/employee_config_server/EmployeeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package employee_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
